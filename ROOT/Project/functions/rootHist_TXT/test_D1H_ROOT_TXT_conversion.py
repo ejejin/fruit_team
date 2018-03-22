091ba22c3cc722e60e244fcdad1ed871c1ebca25
junholeephy <skyblue1293@naver.com>
2018-03-22 17:31:15 +0800

    list readable

---
+++ ROOT/Project/functions/rootHist_TXT/test_D1H_ROOT_TXT_conversion.py
@@ -0,0 +1,12 @@
+#from D1H_ROOT_TXT_conversion import D1H_txt_to_root, D1H_root_to_txt
+from D1H_rootHist_TXT_conversion import D1H_txt_to_roothist, D1H_roothist_to_txt
+
+#ROOT = D1H_txt_to_roothist("./_processed.txt","")
+#TXT= D1H_roothist_to_txt("../junho/py-fillrandom.root",".")
+
+#TXT = D1H_roothist_to_txt("Height1.root","/../");
+TXT = D1H_roothist_to_txt("../../root_generator/root1_gaus.root","~");   #print(type(TXT)); print(TXT);
+ROOT = D1H_txt_to_roothist("root1_gaushist1_F.txt","./");         #print(type(ROOT)); print(ROOT);
+
+
+
