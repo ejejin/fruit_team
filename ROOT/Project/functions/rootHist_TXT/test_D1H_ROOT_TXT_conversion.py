27046d24d983029e4ce0932c08662b034546a616
junholeephy <skyblue1293@naver.com>
2018-03-22 18:06:31 +0800

    test file

--- ROOT/Project/functions/rootHist_TXT/test_D1H_ROOT_TXT_conversion.py
+++ ROOT/Project/functions/rootHist_TXT/test_D1H_ROOT_TXT_conversion.py
@@ -5,9 +5,12 @@ from D1H_rootHist_TXT_conversion import D1H_txt_to_roothist, D1H_roothist_to_txt
 #TXT= D1H_roothist_to_txt("../junho/py-fillrandom.root",".")
 
 #TXT = D1H_roothist_to_txt("Height1.root","/../");
-TXT = D1H_roothist_to_txt("../../root_generator/root1_gaus.root",".");   print(type(TXT)); print(TXT);
+#TXT = D1H_roothist_to_txt("../../root_generator/root1_gaus.root",".");   print(type(TXT)); print(TXT);
 #TXT = D1H_roothist_to_txt("/Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/root_generator/root2_Landau.root",".")
-ROOT = D1H_txt_to_roothist("root2_Landauhist5_F.txt","./");         #print(type(ROOT)); print(ROOT);
+TXT = D1H_roothist_to_txt("/Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/root_generator/root3_sin.root",".")
+
+
+#ROOT = D1H_txt_to_roothist("root2_Landauhist5_F.txt","./");         #print(type(ROOT)); print(ROOT);
 
 
 
