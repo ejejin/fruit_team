6b93599f779a87d28d290dc8b162b0ac9c03a01b
junholeephy <skyblue1293@naver.com>
2018-03-28 14:18:40 +0800

    2D added

--- ROOT/Project/tranfer_test/execute_test.py
+++ ROOT/Project/tranfer_test/execute_test.py
@@ -3,7 +3,10 @@ gBenchmark.Start("All in One")
 import sys
 
 sys.path.append("/Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/functions/rawTxt_Tree_root")
-Infile = "/Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/tranfer_test/data/concrete.txt"
+#Infile = "/Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/tranfer_test/data/concrete.txt"
+#Infile = "/Users/leejunho/Desktop/git/python3Env/group_study/project_pre/data_txt/BINANCE_DATA.txt"
+Infile = "/Users/leejunho/Desktop/git/python3Env/group_study/project_pre/data_txt/BBINA.txt"
+#Infile = "soomin.txt"
 from Raw_text_to_Tree_root import Raw_text_to_Tree_root
 To_Tree = Raw_text_to_Tree_root(Infile,".")
 
@@ -18,9 +21,10 @@ Tree_to_D1H_Components(To_Tree)
 from Tree_to_D1H_CutnGenerate import REGENERATE_TREE_WITH_CUT
 NEW_Tree_PATH = REGENERATE_TREE_WITH_CUT(To_Tree,".")
 
-
+from Tree_to_D2H_Convert import CONVERT_WORKING2D
 from Tree_to_D1H_Convert import CONVERT_WORKING
 HistROOT_PATH = CONVERT_WORKING(NEW_Tree_PATH,"")
+CONVERT_WORKING2D(NEW_Tree_PATH,"")
 
 
 sys.path.append("/Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/functions/rootHist_TXT/func")
@@ -30,7 +34,7 @@ TXT_FILE_LIST =  D1H_roothist_to_txt(HistROOT_PATH, "")
 
 sys.path.append("/Users/leejunho/Desktop/git/python3Env/group_study/project_pre/func")
 from c1_basic_statistic import *
-
+from c2_basic_histo_plotting import Basic_histo
 for Input_file in TXT_FILE_LIST:
     print("The file Name is :",Input_file)
     MODE = most_frequent_bin(Input_file);      print("MODE :",MODE)
@@ -41,6 +45,6 @@ for Input_file in TXT_FILE_LIST:
     VARIANCE = c1_variance(Input_file);        print("VARIANCE :",VARIANCE)
     STD = c1_standard_deviation(Input_file);   print("STD :",STD)
     print("\n")
-
+    Basic_histo(Input_file)
 
 gBenchmark.Show("All in One")
